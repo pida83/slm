d9d5c362231b7b08bd2e016b0e13de8c964fc66c
PC <pida83@naver.com>
2020-01-17 08:47:21 +0900

    test1

--- app/services.php
+++ app/services.php
@@ -11,6 +11,9 @@ use App\Application\Cont\BarManager;
 use Psr\Container\ContainerInterface;
 return function (ContainerInterface $c) {
     // service test
+
+
+    // push to master this
     //$c->set(BarManager::class,\DI\create()->constructor(DI\get(Bar::class),"seconds arg") ); // 생성자에 파라미터 주입 시
     // BarManager::class => \DI\create()->constructor(DI\get(Bar::class),"test") // 생성자에 주입시
 };
