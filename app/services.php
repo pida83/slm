270dbe9e50b645bec497851844b7bc418b35bf7b
PC <pida83@naver.com>
2020-01-17 09:35:01 +0900

    delete all

--- app/services.php
+++ app/services.php
@@ -10,11 +10,6 @@ use App\Application\Inter\Bar;
 use App\Application\Cont\BarManager;
 use Psr\Container\ContainerInterface;
 return function (ContainerInterface $c) {
-    // def
-    // 다 지우기
-    // def
-    // remote push
-    // commit
     //$c->set(BarManager::class,\DI\create()->constructor(DI\get(Bar::class),"seconds arg") ); // 생성자에 파라미터 주입 시
     // BarManager::class => \DI\create()->constructor(DI\get(Bar::class),"test") // 생성자에 주입시
 };
