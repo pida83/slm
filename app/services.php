9b66c4f325f8d1467a926c9abf1656e3729e712b
PC <pida83@naver.com>
2020-01-17 10:01:13 +0900

    change

--- app/services.php
+++ app/services.php
@@ -12,9 +12,4 @@ use Psr\Container\ContainerInterface;
 return function (ContainerInterface $c) {
     //$c->set(BarManager::class,\DI\create()->constructor(DI\get(Bar::class),"seconds arg") ); // 생성자에 파라미터 주입 시
     // BarManager::class => \DI\create()->constructor(DI\get(Bar::class),"test") // 생성자에 주입시
-
-
-
-
-    // re change
 };
